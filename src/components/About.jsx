d838bdd72883bc40e0237a39c5b5d6e42670f06d
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2023-10-15 10:54:15 +0400

    completed about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn('right', 'spring', index * 0.22, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="object-contain w-16 h-16" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p variants={fadeIn('', '', 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        As a seasoned Software Engineer, I have a comprehensive skill set in full stack development for web and mobile
+        applications. My proficiency in .NET, Angular, React, NodeJS, NextJS, SQL Server, MongoDB and React Native
+        allows me to architect and implement robust solutions that meet client requirements. With a track record of
+        delivering successful projects, I also have experience in blockchain and AI development, bringing cutting-edge
+        technologies to a wide range of industries.
+      </motion.p>
 
-export default About
+      <div className="flex flex-wrap gap-10 mt-20">
+        {services.map((service, index) => (
+          <ServiceCard key={index} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, 'about');
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,25 +3,53 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
       <hemisphereLight intensity={1.8} groundColor="black" />
       <pointLight intensity={6} />
       <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={10} castShadow shadow-mapSize={1024} />
-      <primitive object={computer.scene} scale={0.75} position={[0, -4, -1.5]} rotation={[-0.01, -0.2, 0]} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, 0]}
+      />
     </mesh>
   );
 };
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // add event listener for changes to screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    // set the value for isMobile state
+    setIsMobile(mediaQuery.matches);
+
+    // callback function to handle changes in media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // add the callback function as a listener to changes in media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // remove the listener when the component unmounts
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
-      {/* <Suspense> */}
+        {/* <Suspense> */}
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
